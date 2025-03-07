e43b68455f8a3528335ebd12d2d88ecc1336726f
Rishabh Singh <rishabhsingh18071@gmail.com>
2025-03-07 15:02:16 +0530

    Fix java file

--- src/android/MyPlugin.java
+++ src/android/MyPlugin.java
@@ -11,16 +11,25 @@ public class MyPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("openSettings")) {
-            openSettings();
-            callbackContext.success("Settings Opened");
+            openSettings(callbackContext);
             return true;
         }
         return false;
     }
 
-    private void openSettings() {
-        Intent intent = new Intent(Settings.ACTION_SETTINGS);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        cordova.getActivity().startActivity(intent);
+    private void openSettings(CallbackContext callbackContext) {
+        try {
+            Intent intent = new Intent(Settings.ACTION_SETTINGS);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            if (cordova != null && cordova.getActivity() != null) {
+                cordova.getActivity().startActivity(intent);
+                callbackContext.success("Settings Opened");
+            } else {
+                callbackContext.error("Cordova activity is null");
+            }
+        } catch (Exception e) {
+            callbackContext.error("Error opening settings: " + e.getMessage());
+        }
     }
 }
