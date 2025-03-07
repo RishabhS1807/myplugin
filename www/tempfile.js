4bf2e8378fadf4ca8659343c11fbcffd4daeb3c1
Rishabh Singh <rishabhsingh18071@gmail.com>
2025-03-07 14:21:03 +0530

    Removed tempfile.js

--- www/tempfile.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var MyPlugin = {
-    openSettings: function(success, error) {
-        exec(success, error, "MyPlugin", "openSettings", []);
-    }
-};
-
-module.exports = MyPlugin;
